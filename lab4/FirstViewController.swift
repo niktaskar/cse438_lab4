55880cfc160697208c5090ec060e6dd98258c738
Nikash Taskar <nikashtaskar@wustl.edu>
2019-10-20 03:34:49 -0500

    Lots of functionality

--- lab4/FirstViewController.swift
+++ lab4/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController, UICollectionViewDataSource, UISearc
     
     var searchResults:APIResults?
     var movies:[Movie] = []
+    var imageCache:[UIImage] = []
     
 //    var apiURL="https://api.themoviedb.org/3/movie/550?api_key=83e605740e47ab5b794527eb5027f719"
     
@@ -23,47 +24,89 @@ class FirstViewController: UIViewController, UICollectionViewDataSource, UISearc
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+//    @IBOutlet weak var imageView: UIImageView!
+//
+//    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         collectionView.dataSource = self
         searchBar.delegate = self
+        
+        grabData(title: "National Treasure")
+        cacheImages()
     }
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 10
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return movies.count
-        return 2
+        return movies.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "theCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "theCell", for: indexPath) as! MyCollectionViewCell
+        cell.myLabel.text = movies[indexPath.row].title
         
-        cell.backgroundColor = UIColor.blue
-        
+        cell.myImageView.image = imageCache[indexPath.row]
+
         return cell
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        print(searchBar.text)
         grabData(title: searchBar.text ?? "")
     }
     
-    func grabData(title: String){
+    func createQueryString(input: String) -> String {
         var queryString = ""
-        for char in title{
+        for char in input{
             if char == " "{
                 queryString = "\(queryString)+"
             }else {
                 queryString = "\(queryString)\(char)"
             }
         }
-        
+        return queryString
+    }
+    
+    func grabData(title: String){
+        var queryString = createQueryString(input: title)
         let url = "\(baseURL)&query=\(queryString)"
         
+        let finalURL = URL(string: url)
+        
+        let data = try! Data(contentsOf: finalURL!)
+        var searchResults = try! JSONDecoder().decode(APIResults.self, from: data)
+//        print(searchResults)
+        var fullMovieList = searchResults.results
+        var len = 0
+        if fullMovieList.count > 20{
+            len = 20
+        } else {
+            len = fullMovieList.count
+        }
+        
+        for i in 0 ..< len{
+            movies.append(fullMovieList[i])
+        }
+        print(movies)
+        for movie in movies{
+            print(movie.title)
+        }
+        
+    }
+    
+    func cacheImages(){
+        for item in movies{
+            let url = URL(string: item.poster_path!)
+            let data = try? Data.init(contentsOf: url!)
+            let image = UIImage(data: data!)
+            if !imageCache.contains(image!){
+                imageCache.append(image!)
+            }
+        }
     }
     
 }
---
+++ lab4/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  lab4
+//
+//  Created by Nikash Taskar on 10/20/19.
+//  Copyright © 2019 Nikash Taskar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+}
